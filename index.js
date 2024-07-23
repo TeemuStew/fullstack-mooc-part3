4f7b4df531f8014ca500997130b4903ba547968e
TeemuStew <the-makela@hotmail.com>
2024-07-23 19:50:04 +0300

    Varmistus

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(morgan('tiny'));
 
 morgan.token('postData', (req, res) => {
   if (req.method === 'POST') {
@@ -12,29 +18,11 @@ morgan.token('postData', (req, res) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122"
-  }
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
+  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
+  { id: 4, name: "Mary Poppendick", number: "39-23-6423122" }
 ];
-
-app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -51,7 +39,7 @@ app.get('/api/persons/:id', (req, res) => {
   if (person) {
     res.json(person);
   } else {
-    res.status(404).send('Not Found');
+    res.status(404).end();
   }
 });
 
@@ -67,7 +55,7 @@ app.post('/api/persons', (req, res) => {
   }
 
   const newPerson = {
-    id: Math.floor(Math.random() * 100),
+    id: Math.floor(Math.random() * 1000), 
     name: body.name,
     number: body.number
   };
@@ -79,14 +67,12 @@ app.post('/api/persons', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter(person => person.id !== id);
-
   res.status(204).end();
 });
 
 app.get('/info', (req, res) => {
   const requestTime = new Date();
   const infoMessage = `Phonebook has info for ${persons.length} people.<br>${requestTime}`;
-
   res.send(infoMessage);
 });
 
